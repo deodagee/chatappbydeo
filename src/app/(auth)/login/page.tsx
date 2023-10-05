37cc6cce07405474fc6e4d2df361897a1d26e095
deodagee <deos_singiza@hotmail.com>
2023-10-05 07:34:40 -0400

    first commit

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -35,7 +35,10 @@ const Page: FC = () => {
 
             </Image>
             <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
-              Sign in to your account
+              Real Time Chat App
+            </h2>
+            <h2 className='mt-6 text-center text-1xl font-bold tracking-tight text-gray-900'>
+              Sign In To Chat With Your Friends
             </h2>
           </div>
 
